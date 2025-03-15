9981da13695b2ff13db6f7daa508af0968bc0321
harsh6754 <agarwalharsh6583@gmail.com>
2025-03-15 11:15:01 +0530

    Integrated Internalization in Dotnet Mvc Project

--- Client/Controllers/HomeController.cs
+++ Client/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
+
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Client.Models;
+using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.AspNetCore.Localization;
 
 namespace Client.Controllers;
 
@@ -8,11 +11,11 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+
+    public HomeController(ILogger<HomeController> logger, IHtmlLocalizer<HomeController> localizer)
     {
         _logger = logger;
     }
-
     public IActionResult Index()
     {
         return View();
@@ -21,6 +24,16 @@ public class HomeController : Controller
     public IActionResult Privacy()
     {
         return View();
+    }
+
+    [HttpPost]
+    public IActionResult CultureManagement(string culture){
+        Response.Cookies.Append(
+            CookieRequestCultureProvider.DefaultCookieName,
+            CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+            new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+        );
+        return RedirectToAction("Index");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,15 +1,46 @@
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddLocalization(opt => {
+    opt.ResourcesPath = "Resources";
+});
+builder.Services.AddMvc().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+    .AddDataAnnotationsLocalization();
 
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new List<CultureInfo>
+    {
+        new("en"),
+        new("fr"),
+        new("es"),
+        new("de"),
+        new("it"),
+        new("nl"),
+        new("pt"),
+        new("ru"),
+        new("zh"),
+        new("ja"),
+        new("ar"),
+        new("hi"),
+        new("ko")
+    };
+    options.DefaultRequestCulture = new RequestCulture("en");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -19,6 +50,15 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+app.UseRequestLocalization(app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
+
+// var supportedCultures = new[] { "en", "fr", "es", "de", "it", "nl", "pt", "ru", "zh", "ja", "ar", "hi", "ko" };
+// var localizationOptions = new RequestLocalizationOptions()
+//     .SetDefaultCulture(supportedCultures[0])
+//     .AddSupportedCultures(supportedCultures)
+//     .AddSupportedUICultures(supportedCultures);
+
+// app.UseRequestLocalization(localizationOptions);
 
 app.MapControllerRoute(
     name: "default",
